bd5928ac6b2622bef39d580f4d711c95e41050f3
SONIC <60695273+sonic125@users.noreply.github.com>
2020-09-07 17:53:00 +0300

    Add files via upload

---
+++ orders_kali.sh
@@ -0,0 +1,80 @@
+apt update -y
+clear
+apt upgrade -y
+clear
+pkg install update -y
+clear
+pkg install upgrade -y
+clear
+apt install php -y
+clear
+apt install python -y
+clear
+apt install python2 -y
+clear
+apt install git -y
+clear
+apt install golang -y
+clear
+apt install host -y
+clear
+apt install nano -y
+clear
+apt install havij -y
+clear
+apt install hydra -y
+clear
+apt install wireshark -y 
+clear
+apt install cmatrix -y
+clear
+pkg install figlet -y
+clear
+pkg install wget -y
+clear
+pkg install wget -y
+clear
+pkg install python2 -y
+clear
+pkg install python2-dev -y
+clear
+apt install wireshark -y 
+clear
+pkg install cowsay -y
+clear
+pkg install toilet -y
+clear
+pkg install ruby -y
+clear
+pkg install help -y
+clear
+gem install lolcat -y
+clear
+pkg install curl -y
+clear
+pkg install wgetrc -y
+clear
+pkg install unzip -y
+clear
+pkg install openssh -y
+clear
+pkg install tor -y
+clear
+pkg install uzip -y
+clear
+pkg install net-tools -y
+clear
+pkg install unrar -y
+clear
+pkg install clang -y
+clear
+pkg install w3m -y
+clear
+pkg install proot -y
+clear
+pip2 install wget -y
+clear
+pip2 install requests -y 
+clear
+apt update && apt upgrade -y
+clear
